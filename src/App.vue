40e85730837fb9200be30dc489a4a86a816900c6
MohanManirul <fiforeg@gmail.com>
2022-06-06 18:18:05 +0600

    input form data

--- src/App.vue
+++ src/App.vue
@@ -4,80 +4,32 @@
   </header>
 
   <div class="container">
-    <div class="tab">
-      <div class="tab__menu">
-        <!-- render tabs start-->
-        <div  v-for="tab in tabs"
-         :key="tab"
-         class="tab__menu-item"
-         :class="{'tab__menu-item--active' : activeTab === tab}"
-         @click="activeTab = tab"
-         >
-          {{ tab }}
-        </div>
-        <!-- render tabs ends-->
-      
-      </div>
-             
-        <!-- render tab form start -->
-       <div class="tab__content">
-         <br>
-         <!-- <Login v-if="activeTab === 'Login'"/>
-         <Register v-if="activeTab === 'Register'"/> -->
-        <keep-alive>
-        <component :is="activeTab"></component>
-        </keep-alive>            
-       </div>
-        <!-- render tab form ends --->
-    </div>
+    <h1>New User Registration</h1>
+    <label>Full Name</label><br>
+    <input type="text" placeholder="Enter your full name" v-model="formData.firstName">
+  </div>
+  <hr>
+  <label for="">About You</label> <hr/>
+  <textarea v-model="formData.aboutYou"></textarea>
+  <hr>
+
+  <div v-for="(value , key) in formData" :key="key">
+    <strong>{{key}} : </strong> {{ value }}
   </div>
 </template>
 
 <script>
-import ContactDetails from "./ContactDetails.vue";
+
 export default {
   data() {
     return {
-      msg: "Vue3 Vite Project",
-      tabs:["Login" , "Register", "ContactDetails"],
-      activeTab:"Login",
-      name: "Mamunur Rashid",
-      website:"https://rimonbd.com",
-      address:"Dhaka"
+      msg : "Vue 3 bangla Tutorial",
+      formData:{
+        firstName:"",
+        aboutYou : ""
+      }
     };
-  },
-  methods: {},
-  provide(){
-    return{
-      name:this.name,
-      website:this.website,
-      address : this.website
-    }
-  },
-  components: {
-    ContactDetails
   },
 };
 </script>
 
-<style>
-.tab {
-}
-.tab__menu {
-  display: flex;
-}
-
-.tab__menu-item {
-  padding: 6px 15px;
-  background-color: #dbdbdb;
-  cursor: pointer;
-}
-.tab__menu-item--active {
-  color: #fff;
-  background-color: rgb(27, 104, 192);
-}
-input {
-  padding: 5px 11px;
-  margin-bottom: 11px;
-}
-</style>
