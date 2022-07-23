c7376368cefbc3c827636982d0f73da045c388b5
MohanManirul <fiforeg@gmail.com>
2022-07-23 17:55:50 +0600

    data fetch

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
+    <div class="container">
         <h2>Contact List</h2>
+        <hr>
+         <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Designation</th>
+                <th scope="col">Contact No</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr class="table-secondary">
+                <th scope="row">1</th>
+                <th scope="row">1</th>
+                <th scope="row">1</th>
+                <th scope="row">1</th>
+                <th scope="row">1</th>
+            </tr>
+        </tbody>
+    </table>
     </div>
+   
 </template>
 
 <script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,20 @@
     <h3 class="text-center">Vue 3 crud</h3> 
     <hr>
     <div class="container">
-        <p>{{ title }}</p>
-        <ul>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="add_contact">Add Contact</router-link></li>
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Contact List</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/add_contact">Add Contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
 
 </header>
